67c0f278c3adf30aab193a7ad99c88766f1bf73f
IAhA-amalitech <97875943+IAhA-amalitech@users.noreply.github.com>
2022-02-16 21:31:53 +0000

    first working samples; better looking readme; v0.1

--- README.md
+++ README.md
@@ -1,13 +1,39 @@
-random words in array or object
-array for a verb, noun, adjective
+# Mixed Messages Project
+## A Codecademy Pair Programming Project
 
-The <noun> is <verb>
-A <adjective> <noun> was <verb>
-The dog ran
+```Javascript ```
+###  Project Overview
 
-verb(ing) -> singing, playing, listening, dancing, remixing
-verb -> run, walk, talk
+The aim of this project, is to build a _message generator program_. Every time a user runs the program, they should get a new, randomized output. The project is taken in a couple of different forms, like an astrology generator, inspirational message, or nonsensical jokes. To make the program truly random, the message that it outputs is made up of at least three different pieces of data. This project takes what we know of JavaScript syntax so far to build the program and customize it to our liking.
+Project Objectives:
+    
+* Build a message generator program using JavaScript
+* Use Git version control
+* Use command line
+* Develop locally on your computer
 
-noun -> guitar, piano, radio, saxophone, mp3 player, tempo, rhythmn
+Prerequisites:
 
-adjectives -> loud, soft, pleasant, 
+* JavaScript
+* Git and GitHub
+* Command line
+
+
+`random words in array or object`
+OR
+`arrays for a verb, noun, adjective`
+
+Examples of sentences we first developed:
+
+    The <noun> is <verb>
+    A <adjective> <noun> was <verb>
+    The <noun> <verb>
+
+Examples of Nouns, Verbs and Adjectives:
+
+    verb(ing) -> singing, playing, listening, dancing, remixing
+    verb -> run, walk, talk
+
+    noun -> guitar, piano, radio, saxophone, mp3 player, tempo, rhythmn
+
+    adjectives -> loud, soft, pleasant, 
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const verbs = ['singing', 'listening', 'playing', 'dancing', 'remixing']
 const nouns = ['guitar', 'saxophone', 'piano', 'radio', 'tempo', 'rhythm']
-const adjectives = ['loud', 'soft', 'pleasant']
+const adjectives = ['loud', 'soft', 'pleasant', 'inspiring']
 
 // This function randomly selects a verb, noun, and or an adjective
-const grammerSelector = (noun, verb, adjective) => {
-    let randomNoun = noun[Math.floor(Math.random() * noun.length)];
-    let randomVerb = verb[Math.floor(Math.random() * verb.length)];
-    let randomAdjective = adjective[Math.floor(Math.random() * adjective.length)];
+const grammerSelector = () => {
+    let randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+    let randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
+    let randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
     return {
         noun: randomNoun,
         verb: randomVerb,
@@ -14,19 +14,28 @@ const grammerSelector = (noun, verb, adjective) => {
     };
 }
 
+/**
+ *  this line is added to call and generate the random words needed,
+ * without this line grammerSelector object properties are undefined
+ * reference can be seen in test: console.log(grammerSelector.noun);
+ * */ 
+let selectedGrammer = grammerSelector(); 
 
 const genericSenteceA = () => {
-    noun = grammerSelector.noun
-    verb = grammerSelector.verb
+    let noun = selectedGrammer.noun
+    let verb = selectedGrammer.verb
     return `This ${noun} is ${verb}`;
 } 
 
 const genericSentenceB = () => {
-    noun = grammerSelector.noun
-    verb = grammerSelector.verb
-    adjective = grammerSelector.adjective
+    let noun = selectedGrammer.noun
+    let verb = selectedGrammer.verb
+    let adjective = selectedGrammer.adjective
     return `A ${adjective} ${noun} was ${verb}`;
 }
 
 //Testing
 console.log(genericSenteceA())
+// console.log(nouns[Math.floor(Math.random() * nouns.length)]);
+// console.log(verbs[Math.floor(Math.random() * verbs.length)]);
+// console.log(grammerSelector.noun);
