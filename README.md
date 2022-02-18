6341c0824ca0e5b0ac5e00001f266039bcd7d714
IAhA-amalitech <97875943+IAhA-amalitech@users.noreply.github.com>
2022-02-18 12:14:55 +0000

    added arrays to a sentence component object; added getters; added sentence function which uses sentence comp. object and generic sentence functions

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 ## A Codecademy Pair Programming Project
 
 ```Javascript ```
-###  Project Overview
+###  **Project Overview**
 
 The aim of this project, is to build a _message generator program_. Every time a user runs the program, they should get a new, randomized output. The project is taken in a couple of different forms, like an astrology generator, inspirational message, or nonsensical jokes. To make the program truly random, the message that it outputs is made up of at least three different pieces of data. This project takes what we know of JavaScript syntax so far to build the program and customize it to our liking.
+
+***Sentences are not likely to make sense most of the time. :)***
+
 Project Objectives:
     
 * Build a message generator program using JavaScript
@@ -15,14 +18,32 @@ Project Objectives:
 Prerequisites:
 
 * JavaScript
+* Node js
 * Git and GitHub
 * Command line
 
 
-`random words in array or object`
-OR
-`arrays for a verb, noun, adjective`
+<br>
 
+### **Installation**
+Make sure to have **Node.js** installed. No further installations required.
+
+<hr/>
+<br>
+
+### **Execution**
+Navigate to root directory of script.js using a terminal of choice and 
+run the command below.
+
+    node script.js 
+
+
+<br>
+
+<!-- `random words in array or object`
+OR
+`arrays for a verb, noun, adjective` -->
+### ***Examples***
 Examples of sentences we first developed:
 
     The <noun> is <verb>
--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const verbs = ['singing', 'listening', 'playing', 'dancing', 'remixing']
-const nouns = ['guitar', 'saxophone', 'piano', 'radio', 'tempo', 'rhythm']
-const adjectives = ['loud', 'soft', 'pleasant', 'inspiring']
+// const verbs = 
+// const nouns = 
+// const adjectives = 
+
+// optionally, components of the sentence can be sotred in an object
+const sentenceComponents =  {
+    nouns: ['guitar', 'saxophone', 'piano', 'radio', 'tempo', 'rhythm', 'voice'],
+    verbs: ['singing', 'listening', 'playing', 'dancing', 'remixing'],
+    adjectives: ['loud', 'soft', 'pleasant', 'inspiring', 'harmonious'],
+    sentenceTemplates: [
+        "Such a adjective skill in verb noun",
+        "Kofi has a adjective noun"
+    ],
+
+    get _noun (){
+        return this.nouns[Math.floor(Math.random() * this.nouns.length)]
+    },
+    get _verb (){
+        return this.verbs[Math.floor(Math.random() * this.verbs.length)]
+    },
+    get _adjective (){
+        return this.adjectives[Math.floor(Math.random() * this.adjectives.length)]
+    }
+}
 
 // This function randomly selects a verb, noun, and or an adjective
 const grammerSelector = () => {
-    let randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
-    let randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
-    let randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    let randomNoun = sentenceComponents._noun;
+    let randomVerb = sentenceComponents._verb;
+    let randomAdjective = sentenceComponents._adjective;
     return {
         noun: randomNoun,
         verb: randomVerb,
@@ -21,7 +42,7 @@ const grammerSelector = () => {
  * */ 
 let selectedGrammer = grammerSelector(); 
 
-const genericSenteceA = () => {
+const genericSentenceA = () => {
     let noun = selectedGrammer.noun
     let verb = selectedGrammer.verb
     return `This ${noun} is ${verb}`;
@@ -34,8 +55,33 @@ const genericSentenceB = () => {
     return `A ${adjective} ${noun} was ${verb}`;
 }
 
+
+// generate a random message
+const generateSentence = () => {
+    // add generic sentences to sentenceTemplate
+    sentenceComponents.sentenceTemplates.push(genericSentenceA());
+    sentenceComponents.sentenceTemplates.push(genericSentenceB());
+
+    let randomSelector = Math.floor(Math.random() * 4);
+    let sentenceTemplate = sentenceComponents.sentenceTemplates[randomSelector];
+
+    if (sentenceTemplate.includes("adjective")) {
+            sentenceTemplate = sentenceTemplate.replace("adjective",selectedGrammer.adjective)
+        }
+    if (sentenceTemplate.includes("noun")) {
+            sentenceTemplate = sentenceTemplate.replace("noun",selectedGrammer.noun)
+        }
+    if (sentenceTemplate.includes("verb")) {
+            sentenceTemplate = sentenceTemplate.replace("verb",selectedGrammer.verb)
+        }
+        
+    
+    return sentenceTemplate;
+}
+
 //Testing
-console.log(genericSenteceA())
+// console.log(generateSentence())
+console.log(generateSentence())
 // console.log(nouns[Math.floor(Math.random() * nouns.length)]);
 // console.log(verbs[Math.floor(Math.random() * verbs.length)]);
-// console.log(grammerSelector.noun);
+console.log(sentenceComponents.sentenceTemplates);
